33c5a40647cb2f6e54392d8404b36faa7d058117
Matheus Alves <matheusalves.bmd@gmail.com>
2021-07-14 18:45:45 -0300

    Implement the heads or tails game
    Show heads or tails in the result screen when the user clicks the
    "JOGAR" button. Finish the activity of the result screen when the
    user clicks the "VOLTAR" button.

--- app/src/main/java/com/cursoandroid/appcaraoucoroa/MainActivity.java
+++ app/src/main/java/com/cursoandroid/appcaraoucoroa/MainActivity.java
@@ -2,7 +2,10 @@ package com.cursoandroid.appcaraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Notice that MainActivity2 uses the context of this activity
+        // In this case, you could've also used getApplicationContext().
+        ImageButton imageButtonPlay = new ImageButton(this);
+        imageButtonPlay = findViewById(R.id.imageButtonPlay);
+
+        imageButtonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an activity
+                // Here, you cannot use "this" as the context, since the intent creates
+                // a separate Activity and thus a different context
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class );
+
+                // Start the activity
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/cursoandroid/appcaraoucoroa/MainActivity2.java
+++ app/src/main/java/com/cursoandroid/appcaraoucoroa/MainActivity2.java
@@ -3,6 +3,11 @@ package com.cursoandroid.appcaraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageView imageViewResult = findViewById(R.id.imageViewResult);
+
+        int result = new Random().nextInt(2);
+
+        if(result == 0)
+            imageViewResult.setImageResource(R.drawable.moeda_coroa);
+        else
+            imageViewResult.setImageResource(R.drawable.moeda_cara);
+
+        ImageButton imageButtonGoBack = findViewById(R.id.imageButtonGoBack);
+
+        imageButtonGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
